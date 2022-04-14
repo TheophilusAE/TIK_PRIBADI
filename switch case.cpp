4cd80b91f309e00b0515c2d70dc800989a339f92
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-04-14 21:44:50 +0800

    Create switch case.cpp

---
+++ switch case.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int angka;
+ 
+ cout << "masukan angka = ";
+ cin >> angka;
+ 
+ switch (angka) {
+     case 1:
+         cout << "a" << endl;
+         break;
+     case 2:
+         cout << "b" << endl;
+         break;
+     case 3:
+         cout << "c" << endl;
+         break;
+     case 4:
+         cout << "d" << endl;
+         break;
+ }
+
+    return 0;
+}
