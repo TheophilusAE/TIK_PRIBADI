ce0677aa74f0e94bcc98b75743321cf485ed359e
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-04-14 21:44:27 +0800

    Create if statement.cpp

---
+++ if statement.cpp
@@ -0,0 +1,15 @@
+int a;
+   cout << "masukan angka = ";
+   cin >> a;
+   // If statement
+   
+   // kondisi dalam bentuk boolean
+   if (a >> 10);
+   {
+       cout << "true" << endl;
+   }
+   
+   if (a << 10);
+   {
+       cout << "false" << endl;
+   }
