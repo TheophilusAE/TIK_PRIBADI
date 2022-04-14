33408941505a82bb49a15106779fd62e2015f8d0
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-04-14 21:42:15 +0800

    Create Aretmatika.cpp

---
+++ Aretmatika.cpp
@@ -0,0 +1,24 @@
+int a = 10;
+int b = 5;
+
+int hasil;
+
+// penjumlahan
+hasil = a + b;
+cout << a << " + " << b << " = " << hasil << endl;
+
+// pengurangan
+hasil = a - b;
+cout << a << " - " << b << " = " << hasil << endl;
+
+// perkalian
+hasil = a * b;
+cout << a << " X " << b << " = " << hasil << endl;
+
+// pembagian
+hasil = a / b;
+cout << a << " / " << b << " = " << hasil << endl;
+
+// modulus
+hasil = a % b;
+cout << a << " % " << b << " = " << hasil << endl;
