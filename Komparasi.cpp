3d795bf13cf19c1f92cdc6c39c1f5a61bb0e4fe0
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-04-14 21:43:00 +0800

    Create Komparasi.cpp

---
+++ Komparasi.cpp
@@ -0,0 +1,33 @@
+int a = 2;
+ int b = 3;
+
+bool hasil1, hasil2, hasil3, hasil4, hasil5, hasil6;
+
+// komperasi relation
+
+// sebanding ( == ) 
+hasil1 = (a == b);
+cout << hasil1 << endl;
+
+// Tidak sebanding ( != )
+
+hasil2 = (a != b);
+cout << hasil2 << endl;
+
+// lebih dari ( >> )
+
+hasil3 = (a >> b);
+cout << hasil3 << endl;
+
+// kurang dari ( << )
+
+hasil4 = (a << b);
+cout << hasil4 << endl;
+
+// Kurang dari sama dengan ( <= )
+hasil5 = (a <= b);
+cout << hasil5 << endl;
+
+// lebih dari sama dengan ( >= )
+hasil6 = (a >= b);
+cout << hasil6 << endl;
