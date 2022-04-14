1454d3a778c5e8c66d2deaf41fb9297b54f2d540
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-04-14 22:22:10 +0800

    Create Increment dan Decrement.cpp

---
+++ Increment dan Decrement.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+// Increment and Decrement
+
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  int d = 4;
+
+// increment 
+
+// preincrement (++variabel)
+  cout << a << endl;
+  ++a; 
+  cout << a << endl << endl;
+  
+// postincrement (Variabel++)
+  cout << b << endl;
+  b++;
+  cout << b << endl << endl;
+  
+// predecrement (--Variabel)
+  cout << c << endl;
+  --c;
+  cout << c << endl << endl;
+  
+// postdecrement (Variabel--)
+  cout << d << endl;
+  d--;
+  cout << d << endl << endl;
+}
