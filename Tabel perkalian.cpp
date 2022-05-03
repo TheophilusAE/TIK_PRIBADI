facfebc003bda9bb8e1a6df9daaa85e3262cad42
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-05-03 18:56:05 +0800

    Update Tabel perkalian.cpp

--- Tabel perkalian.cpp
+++ Tabel perkalian.cpp
@@ -7,11 +7,9 @@ int main()
     int a,b,c;
     cout << "masukan angka: ";
     cin >> a;
+   
     
-    cout << "masukan batas perkalian: ";
-    cin >> c;
-    
-    for ( b = 0 ; b <= c  ; ++b) {
+    for ( b = 0 ; b <= 10  ; ++b) {
         cout << a << " * " << b << " = " << a*b << endl;
     }
 
