124b4666d208d72a5b133480f55bfdb13b38492b
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-05-09 18:52:40 +0800

    Create Barisan fibonaci.cpp

---
+++ Barisan fibonaci.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int f_n;
+    int f_n1;
+    int f_n2;
+    
+    cout << "PROGRAM DERET FIBONACI \n";
+    cout << "Masukan nilai ke-n: ";
+    cin >> n;
+    
+    f_n1 = 1;
+    f_n2 = 0;
+    f_n = f_n1 + f_n2;
+    cout << f_n << " ";
+    
+    for (int i = 1; i < n; i++) {
+        f_n = f_n1 + f_n2;
+        f_n2 = f_n1;
+        f_n1 = f_n;
+        cout << f_n << " ";
+    }
+    cout << "\n";
+    
+    
+    return 0;
+}
