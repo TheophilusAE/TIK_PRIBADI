fda30dd720a11848757bbce504cfc3db6b89e99d
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-05-10 20:27:44 -0700

    Create tyugas kasar.cpp

---
+++ tyugas kasar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// array 2 dimensi
+
+int main()
+{
+   int a [2][2] = {
+   };
+   
+   int a [2][2] = {
+   };
+   
+   for (int i = 0; i < 5; i++){
+       for (int j = 0; j < 4; j++){
+          cout << int a [i][j] + b [][]
+       }
+   }
+    return 0;
+}
