3ccf66890d6195072d174dedff8772ace23c2640
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-05-19 21:15:36 +0800

    Create fungsi 1 .cpp

---
+++ fungsi 1 .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x;
+    
+    cout << "masukan angka: ";
+    cin >> x;
+    
+    float y = sqrt (x);
+    
+    cout << "akar dari " << x <<" adalah : " << y << endl;
+
+    return 0;
+}
