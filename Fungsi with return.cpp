e6d4aa4f1386f69ec2c2287e582c13ce5a72dd0c
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-06-02 21:36:55 +0800

    Update Fungsi with return.cpp

--- Fungsi with return.cpp
+++ Fungsi with return.cpp
@@ -30,16 +30,34 @@ int pengurangan (int d, int e) {
     return f;
 }
 
+int perkalian (int f, int g) {
+    int h;
+    h = f * g;
+    return h;
+}
+
+int pembagian (int i, int j) {
+    int k;
+    k = i / j;
+    return k;
+}
+
+int pembagian2 (int l,int m) {
+    int n;
+    n = l % m;
+    return n;
+}
+
 int main() {
     string pilihan;
     
     cout << "----------selamat datang-----------" << endl;
     cout << "\n";
    
-    cout << "(penjumlahan/pengurangan/pangkat)\n";
+    cout << "(penjumlahan/pengurangan/pangkat/perkalian/pembagian/modulus)\n";
     cout << "pilih operasi hitung : ";
     cin >> pilihan;
-    cout << "\n\n";
+    cout << "\n";
     if (pilihan == "pangkat") {
         int x,hasil;
         cout << "nilai kuadrat dari : ";
@@ -68,7 +86,35 @@ int main() {
         hasil3 = pengurangan (a,b);
         cout << "hasil pengurangan dari " << a << "-" << b << " adalah " << hasil3;
     }
-
-
+    else if (pilihan == "perkalian") {
+        int a,b,hasil4;
+        cout << "masukan nilai a: ";
+        cin >> a;
+        cout << "masukan nilai b: ";
+        cin >> b;
+        
+        hasil4 = perkalian (a,b);
+        cout << "hasil perkalian dari " << a << "x" << b << " adalah " << hasil4;
+    }
+    else if (pilihan == "pembagian") {
+        int a,b,hasil5;
+        cout << "masukan nilai a: ";
+        cin >> a;
+        cout << "masukan nilai b: ";
+        cin >> b;
+        
+        hasil5 = pembagian (a,b);
+        cout << "hasil pembagian dari " << a << ":" << b << " adalah " << hasil5;
+    }
+    else if (pilihan == "modulus") {
+         int a,b,hasil6;
+        cout << "masukan nilai a: ";
+        cin >> a;
+        cout << "masukan nilai b: ";
+        cin >> b;
+        
+        hasil6 = pembagian2 (a,b);
+        cout << "hasil modulus dari " << a << "%" << b << " adalah " << hasil6;
+    }
     return 0;
 }
